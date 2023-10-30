c05eba2feeff0c47d67753648887819b871d8e52
tr1sm0s1n <mobinm.mohanan@gmail.com>
2023-10-30 20:05:44 +0530

    ✨ | added lesson 05

---
+++ 05-primitive_types/primitive_types1.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    let is_morning = true;
+    if is_morning {
+        println!("Good morning!");
+    }
+
+    let is_evening = false;
+    if is_evening {
+        println!("Good evening!");
+    }
+}
