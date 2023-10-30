e835ae1008c8d6c8eee128aece105bff85a91118
tr1sm0s1n <mobinm.mohanan@gmail.com>
2023-10-30 20:06:06 +0530

    ✨ | added lesson 06

---
+++ 06-vecs/vecs2.rs
@@ -0,0 +1,36 @@
+fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
+    for element in v.iter_mut() {
+        *element = *element * 2
+    }
+
+    v
+}
+
+fn vec_map(v: &Vec<i32>) -> Vec<i32> {
+    v.iter()
+        .map(|element| {
+            element * 2
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_vec_loop() {
+        let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
+        let ans = vec_loop(v.clone());
+
+        assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
+    }
+
+    #[test]
+    fn test_vec_map() {
+        let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
+        let ans = vec_map(&v);
+
+        assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
+    }
+}
