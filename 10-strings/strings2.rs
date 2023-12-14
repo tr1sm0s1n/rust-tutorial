f0a45a30fbc4ddeb68ccbf5dab82de8cc62042cc
tr1sm0s1n <mobinm.mohanan@gmail.com>
2023-12-14 20:56:40 +0530

    ✨ | added lesson 10

---
+++ 10-strings/strings2.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let word = String::from("green");
+    if is_a_color_word(&word.to_string()) {
+        println!("That is a color word I know!");
+    } else {
+        println!("That is not a color word I know.");
+    }
+}
+
+fn is_a_color_word(attempt: &str) -> bool {
+    attempt == "green" || attempt == "blue" || attempt == "red"
+}
