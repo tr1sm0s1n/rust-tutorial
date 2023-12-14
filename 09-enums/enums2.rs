3e095afba13039368f3137bd7b56dbe27f0b9ed9
tr1sm0s1n <mobinm.mohanan@gmail.com>
2023-12-14 20:56:23 +0530

    ✨ | added lesson 09

---
+++ 09-enums/enums2.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+enum Message {
+    Move { x: u8, y: u8 },
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", self);
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Move { x: 10, y: 30 },
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
